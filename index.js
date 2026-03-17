08231ef94cef1d03bf996c6ec48f4adeb58ee2dc
Indrasena reddy <88037874+indrasenareddy91@users.noreply.github.com>
2026-03-18 00:11:05 +0530

    Nn

--- index.js
+++ index.js
@@ -8,26 +8,35 @@ const port = process.env.PORT || 3000;
 // Resolve directory name (since ES modules don't have __dirname)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 app.get("/c", (req, res) => {
   res.sendFile(path.join(__dirname, "c.txt"));
+});
+
 app.get("/f", (req, res) => {
   res.sendFile(path.join(__dirname, "f.txt"));
-});app.get("/s", (req, res) => {
+});
+
+app.get("/s", (req, res) => {
   res.sendFile(path.join(__dirname, "s.txt"));
 });
-// Route to serve the file
+
 app.get("/ds", (req, res) => {
   res.sendFile(path.join(__dirname, "ds.txt"));
 });
+
 app.get("/eh", (req, res) => {
   res.sendFile(path.join(__dirname, "eh.txt"));
 });
+
 app.get("/v", (req, res) => {
   res.sendFile(path.join(__dirname, "v.txt"));
 });
+
 app.get("/h", (req, res) => {
   res.sendFile(path.join(__dirname, "h.txt"));
 });
+
 app.get("/a", (req, res) => {
   res.sendFile(path.join(__dirname, "a.txt"));
 });
@@ -35,18 +44,23 @@ app.get("/a", (req, res) => {
 app.get("/dc", (req, res) => {
   res.sendFile(path.join(__dirname, "dc.txt"));
 });
+
 app.get("/d", (req, res) => {
   res.sendFile(path.join(__dirname, "d.txt"));
 });
+
 app.get("/xor", (req, res) => {
   res.sendFile(path.join(__dirname, "xor.txt"));
 });
+
 app.get("/k", (req, res) => {
   res.sendFile(path.join(__dirname, "k.txt"));
 });
+
 app.get("/mx", (req, res) => {
   res.sendFile(path.join(__dirname, "mx.txt"));
 });
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
