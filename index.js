105b77356cce97dc8f924d421317d2d5b33550af
Indrasena reddy <88037874+indrasenareddy91@users.noreply.github.com>
2026-04-06 11:20:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const routes = {
+  "/g":"g.py",
   "/e": "e.py",
   "/r": "randomized_response.py",
   "/l": "laplace-updated-credit.py",
