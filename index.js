72a807e14930645215f9157ca05a1228c1478cdf
Indrasena reddy <88037874+indrasenareddy91@users.noreply.github.com>
2026-04-06 11:00:58 +0530

    Jeheh

--- index.js
+++ index.js
@@ -5,69 +5,24 @@ import { fileURLToPath } from "url";
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Resolve dhirectory name (since ES modules don't have __dirname)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.get("/c", (req, res) => {
-  res.sendFile(path.join(__dirname, "c.txt"));
-});
+const routes = {
+  "/e": "exponential_mech.ipynb",
+  "/r": "randomized_response.py",
+  "/l": "laplace-updated-credit.py",
+  "/t": "t-closeness.py",
+  "/x": "x-y anonymity and linkability.py",
+  "/k": "k-anonymity.py",
+  "/d": "l-diversity.py",
+};
 
-app.get("/dl", (req, res) => {
-  res.sendFile(path.join(__dirname, "dl.txt"));
-});
-
-app.get("/s", (req, res) => {
-  res.sendFile(path.join(__dirname, "s.txt"));
-});
-
-app.get("/ds", (req, res) => {
-  res.sendFile(path.join(__dirname, "ds.txt"));
-});
-
-app.get("/eh", (req, res) => {
-  res.sendFile(path.join(__dirname, "eh.txt"));
-});
-
-app.get("/v", (req, res) => {
-  res.sendFile(path.join(__dirname, "v.txt"));
-});
-
-app.get("/h", (req, res) => {
-  res.sendFile(path.join(__dirname, "h.txt"));
-});
-
-app.get("/a", (req, res) => {
-  res.sendFile(path.join(__dirname, "a.txt"));
-});
-
-app.get("/e", (req, res) => {
-  res.sendFile(path.join(__dirname, "e.txt"));
-});
-
-app.get("/d", (req, res) => {
-  res.sendFile(path.join(__dirname, "d.txt"));
-});
-
-app.get("/xor", (req, res) => {
-  res.sendFile(path.join(__dirname, "xor.txt"));
-});
-
-app.get("/des", (req, res) => {
-  res.sendFile(path.join(__dirname, "des.txt"));
-});
-
-app.get("/k", (req, res) => {
-  res.sendFile(path.join(__dirname, "k.txt"));
-});
-
-app.get("/mx", (req, res) => {
-  res.sendFile(path.join(__dirname, "mx.txt"));
-});
-
-app.get("/m", (req, res) => {
-  res.sendFile(path.join(__dirname, "m.txt"));
-});
+for (const [route, file] of Object.entries(routes)) {
+  app.get(route, (req, res) => {
+    res.download(path.join(__dirname, file));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
